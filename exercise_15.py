c02ae01dfae502f1c9a63fd4b4d048040c4f3287
ZeBonanaMan <mcoygaldiano@gmail.com>
2024-01-26 20:34:49 +0800

    first commit

---
+++ exercise_15.py
@@ -0,0 +1,9 @@
+def exponent(base, exp):
+    result = base ** exp 
+    return result
+
+base_value = 2
+exponent_value = 5
+
+result = exponent(base_value, exponent_value)
+print(base_value, "raises to the power of", exponent_value, "is:", result)
